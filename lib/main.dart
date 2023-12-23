8bc8addefb2efb004ff911b37a8a3217450a5467
RJ2607 <rishijain2607@gmail.com>
2023-12-24 01:25:58 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
@@ -15,27 +17,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-				future: _determinePosition(),
-        builder: (context, snap) {
-					if(snap.hasData) {
-						return BlocProvider<WeatherBlocBloc>(
-							create: (context) => WeatherBlocBloc()..add(
-								Fetchweather(snap.data as Position)
-							),
-							child: MyHome(),
-						);
-					} else {
-						return Scaffold(
-							body: Center(
-								child: CircularProgressIndicator(),
-							),
-						);
-					}
-        }
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+            future: _determinePosition(),
+            builder: (context, snap) {
+              if (snap.hasData) {
+                return BlocProvider<WeatherBlocBloc>(
+                  create: (context) => WeatherBlocBloc()
+                    ..add(Fetchweather(snap.data as Position)),
+                  child: MyHome(),
+                );
+              } else {
+                return Scaffold(
+                    backgroundColor: Colors.black,
+                    extendBodyBehindAppBar: true,
+                    // appBar: AppBar(
+                    //   backgroundColor: Color.fromARGB(0, 255, 255, 255),
+                    //   elevation: 8,
+                    //   systemOverlayStyle: const SystemUiOverlayStyle(
+                    //     statusBarBrightness: Brightness.dark,
+                    //   ),
+                    // ),
+                    body: Padding(
+                        padding: const EdgeInsets.fromLTRB(
+                            40, 1.2 * kToolbarHeight, 40, 20),
+                        child: SizedBox(
+                            height: MediaQuery.of(context).size.height,
+                            child: Stack(children: [
+                              Align(
+                                alignment: const AlignmentDirectional(3, -0.2),
+                                child: Container(
+                                  height: 300,
+                                  width: 600,
+                                  decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 111, 52, 212),
+                                      shape: BoxShape.rectangle),
+                                ),
+                              ),
+                              Align(
+                                alignment: const Alignment(0, -1.2),
+                                child: Container(
+                                  height: 250,
+                                  width: 600,
+                                  decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 245, 192, 57),
+                                      shape: BoxShape.rectangle),
+                                ),
+                              ),
+                              BackdropFilter(
+                                filter:
+                                    ImageFilter.blur(sigmaX: 70, sigmaY: 70),
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                      color: Colors.transparent),
+                                ),
+                              ),
+                              Center(
+                                  child: CircularProgressIndicator(
+                                color: Colors.white,
+                              ))
+                            ]))));
+              }
+            }));
   }
 }
 
@@ -47,7 +90,7 @@ Future<Position> _determinePosition() async {
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
     // Location services are not enabled don't continue
-    // accessing the position and request users of the 
+    // accessing the position and request users of the
     // App to enable the location services.
     return Future.error('Location services are disabled.');
   }
@@ -58,18 +101,18 @@ Future<Position> _determinePosition() async {
     if (permission == LocationPermission.denied) {
       // Permissions are denied, next time you could try
       // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale 
+      // Android's shouldShowRequestPermissionRationale
       // returned true. According to Android guidelines
       // your App should show an explanatory UI now.
       return Future.error('Location permissions are denied');
     }
   }
-  
+
   if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
+    // Permissions are denied forever, handle appropriately.
     return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
 
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
